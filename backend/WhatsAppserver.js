8c74adbb48dde1aed471ed3130b9d81fd4cda444
Ajay kumar <aajaykumarr32@gmail.com>
2024-10-13 21:25:21 +0530

    change in twilio

--- backend/WhatsAppserver.js
+++ backend/WhatsAppserver.js
@@ -8,9 +8,14 @@ const port = 4000;
 app.use(bodyParser.json());
 app.use(cors());
 require("dotenv").config();
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = twilio(accountSid, authToken);
+console.log("process.env ", process.env);
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+const TWILIO_ACCOUNT_SID = "AC1feaa08768440ee10733bf0e3b9ceb3c";
+const TWILIO_AUTH_TOKEN = "705ff897c918cc901a0d13034cf378c2";
+// console.log("accountSid ", accountSid);
+// console.log("authToken ", authToken);
+const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 // const twilioNumber = '+12406509853';
 const twilioNumber = "+14155238886";
 app.post("/send-whatsapp", (req, res) => {
@@ -23,21 +28,17 @@ app.post("/send-whatsapp", (req, res) => {
     })
     .then((message) => {
       console.log(`Message sent with SID: ${message.sid}`);
-      res
-        .status(200)
-        .json({
-          success: true,
-          message: `Message sent with SID: ${message.sid}`,
-        });
+      res.status(200).json({
+        success: true,
+        message: `Message sent with SID: ${message.sid}`,
+      });
     })
     .catch((error) => {
       console.log(`Error sending message: ${error.message}`);
-      res
-        .status(500)
-        .json({
-          success: false,
-          message: `Error sending message: ${error.message}`,
-        });
+      res.status(500).json({
+        success: false,
+        message: `Error sending message: ${error.message}`,
+      });
     });
 });
 app.listen(port, () => {
