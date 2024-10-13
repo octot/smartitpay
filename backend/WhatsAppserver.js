1ad47e0979b1270c3cb965f674c3cf5b590bc445
Ajay kumar <aajaykumarr32@gmail.com>
2024-10-13 20:05:59 +0530

    Use environment variables for Twilio secrets

--- backend/WhatsAppserver.js
+++ backend/WhatsAppserver.js
@@ -1,32 +1,43 @@
 // const console = require('./logger');
-const express = require('express');
-const bodyParser = require('body-parser');
-const twilio = require('twilio');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const twilio = require("twilio");
+const cors = require("cors");
 const app = express();
 const port = 4000;
 app.use(bodyParser.json());
 app.use(cors());
-const accountSid = 'AC1feaa08768440ee10733bf0e3b9ceb3c';
-const authToken = '705ff897c918cc901a0d13034cf378c2';
+require("dotenv").config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 // const twilioNumber = '+12406509853';
-const twilioNumber = '+14155238886';
-app.post('/send-whatsapp', (req, res) => {
+const twilioNumber = "+14155238886";
+app.post("/send-whatsapp", (req, res) => {
   const { message, to } = req.body;
   client.messages
     .create({
       body: message,
       from: `whatsapp:${twilioNumber}`,
-      to: `whatsapp:${to}`
+      to: `whatsapp:${to}`,
     })
-    .then(message => {
+    .then((message) => {
       console.log(`Message sent with SID: ${message.sid}`);
-      res.status(200).json({ success: true, message: `Message sent with SID: ${message.sid}` });
+      res
+        .status(200)
+        .json({
+          success: true,
+          message: `Message sent with SID: ${message.sid}`,
+        });
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(`Error sending message: ${error.message}`);
-      res.status(500).json({ success: false, message: `Error sending message: ${error.message}` });
+      res
+        .status(500)
+        .json({
+          success: false,
+          message: `Error sending message: ${error.message}`,
+        });
     });
 });
 app.listen(port, () => {
