7fd70a972eff2e3fd6c780256c6c311ad5005696
ajaykumardev32 <ajayk@dlt.sg>
2025-06-22 15:44:59 +0530

    CHANGES FOR PORT

--- backend/WhatsAppserver.js
+++ backend/WhatsAppserver.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const twilio = require("twilio");
 const cors = require("cors");
 const app = express();
-const port = 4000;
+const PORT = process.env.PORT || 4000;
 app.use(bodyParser.json());
 app.use(cors());
 require("dotenv").config();
@@ -11,6 +11,9 @@ const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
 const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 const twilioNumber = "+14155238886";
+app.get("/", (req, res) => {
+  res.send("Hello from WhatsAppserver!");
+});
 app.post("/send-whatsapp", (req, res) => {
   const { message, to } = req.body;
   client.messages
@@ -20,20 +23,20 @@ app.post("/send-whatsapp", (req, res) => {
       to: `whatsapp:${to}`,
     })
     .then((message) => {
-      
+
       res.status(200).json({
         success: true,
         message: `Message sent with SID: ${message.sid}`,
       });
     })
     .catch((error) => {
-      
+
       res.status(500).json({
         success: false,
         message: `Error sending message: ${error.message}`,
       });
     });
 });
-app.listen(port, () => {
-  
+app.listen(PORT, () => {
+  console.log("Connnecte to ", PORT)
 });
